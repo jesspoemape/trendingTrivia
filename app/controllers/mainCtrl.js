7bd9059bc95344143f0a14d0fa33e4995950af2b
Jess Poemape <jesspoemape@gmail.com>
2017-08-03 12:38:16 -0600

    clicking on options returns a response from the mains ervice in the console

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,3 +1,7 @@
 angular.module('trivia').controller('mainCtrl', function($scope, mainSrv) {
     $scope.test = 'hello world'
+    $scope.getQuestions = mainSrv.getQuestions().then(data => $scope.questions = data);
+    $scope.checkAnswer = function(index, correct) {
+        mainSrv.checkAnswer(index, correct);
+    }
 })
--- app/services/mainService.js
+++ app/services/mainService.js
@@ -1,3 +1,13 @@
-angular.module('trivia').service('mainSrv', function() {
-    
+angular.module('trivia').service('mainSrv', function($http) {
+    this.getQuestions = function() {
+        return $http.get('https://practiceapi.devmountain.com/api/trivia/questions').then(res => res.data).catch(console.error, 'Error');
+    }
+    this.checkAnswer = function(index, correct) {
+        if (index === correct) {
+            console.log('good job');
+        }
+        else {
+            console.log('you suck');
+        }
+    }
 })
